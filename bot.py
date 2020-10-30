d8f2a11fb6082eb972c52bd5a7e00352822cf070
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-10-30 22:02:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,13 +74,7 @@ def echo(update, context):
             update.message.reply_text(' Status : ' + r['status'])
             update.message.reply_text(' shortenedUrl : ' + r['shortenedUrl'])
         if r['status'] == 'Sorry something went wrong pleas try again 🙏':
-            update.message.reply_text(' Error : ' + r['message'])
-                     
-            
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Please dont spam here\ni will ban you")
-
-unknown_handler = MessageHandler(Filters.command, unknown, echo)            
+            update.message.reply_text(' Error : ' + r['message'])           
             
 def main():
     updater = Updater(
@@ -91,7 +85,6 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_command))
     dp.add_handler(CommandHandler("auth", auth))
-    dp.add_handler(unknown_handler)
                               
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
     updater.start_polling()  
