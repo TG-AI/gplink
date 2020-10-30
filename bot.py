588f5d81e669740e037d82b0d64c64f2c288362d
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-10-30 15:56:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,7 +94,7 @@ def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
+    dp.add_handler(unknown_handler)
     
       
 
