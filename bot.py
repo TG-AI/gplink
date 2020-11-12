57dca21105a7bb4029c6545aaf006464a465e7cb
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-11-13 01:02:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,12 +65,8 @@ def ech(update: Update, context: CallbackContext):
         with open('gplink_tokens.py', 'w') as file:
             file.write('tokensp = ' + str(tokensp))
             update.message.reply_text(f'🎉 congratulations \n\nYour 😇 CHAT_ID : {chat} IS REGISTERED WITH GPLINK API TOKEN : {tokenp}\n\nIf you sent me a different API URL I will reassign your GPLINK API TOKEN')
-    
-    return eco            
-
-def eco(update: Update, context: CallbackContext):
             
-   if 'https://golinksrt.xyz/api?api=' or 'https://gplinks.in/api?api=' not in str(update.message.text) and (re.search('^http://.*', str(update.message.text)) or re.search('^https://.*', str(update.message.text))):
+   elif 'https://golinksrt.xyz/api?api=' or 'https://gplinks.in/api?api=' not in str(update.message.text) and (re.search('^http://.*', str(update.message.text)) or re.search('^https://.*', str(update.message.text))):
         
     keyboard = [
             [
